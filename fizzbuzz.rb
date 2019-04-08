461a731e0a8266d3b0fbf2f4844b7783610d2910
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-08 03:17:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require 'pry'
 def fizzbuzz(int)
   if int % 3 && int % 5
     "FizzBuzz"
@@ -9,5 +10,6 @@ def fizzbuzz(int)
     "Fizz"
   else int
     nil
+  binding.pry  
   end  
 end  
